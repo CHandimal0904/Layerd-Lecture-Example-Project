c75d7eb24677a4137ac17b059680e2d99ca2a2d6
Chandimal <chandimalnimantha1119@gmail.com>
2023-06-17 09:19:12 +0530

    add save customer to DAOImpl

--- src/DAO/CustomerDAOImpl.java
+++ src/DAO/CustomerDAOImpl.java
@@ -3,10 +3,7 @@ package DAO;
 import db.DBConnection;
 import model.CustomerDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl  {
@@ -22,5 +19,13 @@ public class CustomerDAOImpl  {
             allCustomer.add(new CustomerDTO(id,name,address));
         }
         return allCustomer;
+    }
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        return pstm.executeUpdate() > 0;
     }
 }
