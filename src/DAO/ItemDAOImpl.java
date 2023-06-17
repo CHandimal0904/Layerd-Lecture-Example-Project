c8089826b6c5f6039dcfea30a8c6b116e1e11313
Chandimal <chandimalnimantha1119@gmail.com>
2023-06-17 11:38:35 +0530

    refactor saveitem to ItemDAOImpl

--- src/DAO/ItemDAOImpl.java
+++ src/DAO/ItemDAOImpl.java
@@ -28,5 +28,14 @@ public class ItemDAOImpl {
         pstm.setString(1, code);
         return pstm.executeUpdate() > 0;
     }
+    public boolean saveItem(ItemDTO item) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, item.getCode());
+        pstm.setString(2, item.getDescription());
+        pstm.setBigDecimal(3, item.getUnitPrice());
+        pstm.setInt(4, item.getQtyOnHand());
+        return pstm.executeUpdate() >0;
+    }
 
 }
